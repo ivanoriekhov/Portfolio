014232464338121f330f8e31e394b510a7381beb
ivanoriekhov <ivanoriekhov1999@gmail.com>
2026-09-03 10:37:57 -0400

    Update queries.sql

--- projects/SQL-security-analysis/scenario 5/queries.sql
+++ projects/SQL-security-analysis/scenario 5/queries.sql
@@ -1,1 +1,42 @@
+-- ============================================================
+-- SQL security analysis
+-- Google Cybersecurity Certificate Portfolio Activity
+-- SCENARIO 5: SQL Inner and outer join
+-- Database: organization
+-- Tables: machines, employees
+-- ============================================================
+
+
+
+-- ------------------------------------------------------------
+-- Task 1: Retrieve employee information
+-- ------------------------------------------------------------
+SELECT *
+FROM employees; 
+INNER JOIN employees ON machines.device_id = employees.device_id;
+
+
+-- ------------------------------------------------------------
+-- Task 2: Retrieve more data
+-- ------------------------------------------------------------
+
+SELECT * 
+FROM machines 
+LEFT JOIN employees ON machines.device_id = employees.device_id;
+
+SELECT * 
+FROM machines
+RIGHT JOIN employees ON machines.device_id = employees.device_id;
+
+
+-- ------------------------------------------------------------
+-- Task 3: Join employees with login attempts
+-- ------------------------------------------------------------
+-- The username column is common to both tables. The INNER JOIN returns records where a username exists in both tables.
+
+SELECT *
+FROM employees
+INNER JOIN log_in_attempts ON employees.username = log_in_attempts.username;
+
+
 
